a10120e8e58d6068465a9d651fdcd91ba04827fd
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-02-24 12:23:17 +0100

    arrays

--- chapitre-06/index.js
+++ chapitre-06/index.js
@@ -2,7 +2,7 @@
 let index = 5;
 
 while (index < 5) {
-  console.log(index);
+  // console.log(index);
   index = index + 1;
 }
 
@@ -10,18 +10,48 @@ while (index < 5) {
 let counter = 5;
 
 do {
-  console.log(counter);
+  // console.log(counter);
   counter = counter + 1;
 } while(counter < 5);
 
 // La boucle for
 for (let index = 0; index < 5; index++) {
-  console.log(index);
+  // console.log(index);
 }
 
 // 5
-console.log(index);
+// console.log(index);
 
 // Exercice : calculer la validité d'un numéro SIREN
 
-let sirenNumber = 732829320;
+let sirenNumber = "732829320";
+let total = 0;
+
+const sirenNumberLength = sirenNumber.length;
+
+for (let index = 0; index < sirenNumberLength; index++) {
+  const character = sirenNumber[index];
+
+  let digit = Number(character);
+
+  if (index % 2 !== 0) {
+    digit *= 2;
+
+    const string = String(digit);
+    const numberOfDigits = string.length;
+
+    let sum = 0;
+
+    if (numberOfDigits > 1) {
+      for (let characterIndex = 0; characterIndex < numberOfDigits; characterIndex++) {
+        sum += Number(string[characterIndex]);
+      }
+
+      digit = sum;
+    }
+  }
+
+  total += digit;
+}
+
+console.log(total % 10 === 0);
