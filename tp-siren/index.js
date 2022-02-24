1f2abd99af69c36ae3cdbe1ea8c83ae0d76d72a1
jeremdh62 <jeremdh.dev@outlook.com>
2022-02-24 11:32:53 +0100

    tp code siren fait

---
+++ tp-siren/index.js
@@ -0,0 +1,24 @@
+let siren = "732829320";
+
+let result = 0;
+
+for(let i = 0; i < siren.length; i++ ) {
+    
+    if(i % 2 != 0) {
+        let newNum = parseInt( siren.charAt(i) ) * 2;
+
+        for (let i2 = 0 ; i2 < newNum.toString().length; i2++) {
+            result = result + parseInt(newNum.toString().charAt(i2));
+        }
+        
+    } else {
+        result = result + parseInt( siren.charAt(i) )  ;
+    }
+    
+}
+
+if (result % 10 == 0) {
+    console.log("Le siren est correct");
+} else {
+    console.log("Le siren est incorrect");
+}
