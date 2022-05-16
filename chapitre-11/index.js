5411938c666012732e78b4a5270f0499db56a748
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-05-16 14:44:56 +0200

    added exercise

--- chapitre-11/index.js
+++ chapitre-11/index.js
@@ -1,8 +1,20 @@
 "use strict";
 
-console.log("Erreurs");
-
 // Rattraper des erreurs
+
+const divide = (x, y) => x / y;
+
+try {
+  console.log(`1 / 2 = ${divide(1, 0)}`);
+} catch (error) {
+  console.log(`Erreur: ${error.message}`);
+}
+
 // Lever des exceptions
 // Finalisation de rattrapage d'erreurs
-// Exercice : sécurisation d'une fonction
+// Exercice : fonction qui prend en paramètre deux nombre et renvoi la division des deux nombres
+// 1. Vérifiez que tous les paramètres soient des nombres (pas de Infinity et pas de NaN)
+// 2. Vérifier que tous les paramètres soient des entiers
+// 3. Le deuxième paramètre ne soit pas zéro
+// 4. Pour tous les types d'erreurs je veux que vous construisiez des fonctions constructrices
+// 5. Renvoyer le résultat
