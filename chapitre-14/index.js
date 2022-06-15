1e4867c05948d6a1209d3d0a87456f41169d1be6
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-06-15 18:27:08 +0200

    updated chapter 14

--- chapitre-14/index.js
+++ chapitre-14/index.js
@@ -2,6 +2,20 @@
 
 console.log("chapitre 14");
 
-// Fonction asynchrone
-// Levée et récupération d'erreur à l'intérieur du fonction asynchrone
-// Exercice : reprendre l'exercice du chapitre précédent et utiliser les mot-clés async/await
+// Document
+// Récupération d'un élément HTML
+// Lecture d'un attribut
+// Ajout d'un attribut
+// Modification d'un attribut
+// Suppression d'un attribut
+// Ajout d'une classe CSS
+// Suppression d'une classe CSS
+// Lecture des enfants
+// Ajout d'un enfant
+// Suppression d'un enfant
+// Remplacement d'un enfant
+// À faire : Créer un formulaire permettant d'ajouter des membres à un tableau de membre
+// Chaque membre est constitué d'un nom, prénom et d'une adresse email
+// Chaque membre peut être supprimé
+// Chaque membre peut être modifié (par défaut il est en lecture seule)
+// Lorsqu'un membre passe en état modification, chacune de ses colonnes se transforme en champ de texte
